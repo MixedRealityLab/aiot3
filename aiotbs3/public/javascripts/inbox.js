57bf605c509e612d323df8d9240c247ed7238a73
Carolina <carolina.fuentes@nottingham.ac.uk>
2017-12-04 16:17:56 +0000

    inbox notifications

---
+++ aiotbs3/public/javascripts/inbox.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function() {
+    var table = $('#beforeOut').DataTable( {
+        "bFilter": false,
+        "bInfo": false,
+        "ajax": "../ajax/data/objects.txt",
+        "columns": [
+            {
+                "className":  false
+            },
+            { "data": "name" }
+        ],
+        "lengthChange": false,
+        "length": 10,
+        "paging": false,
+        "destroy": true,
+        "scrollY": '150px'
+    } );
+
+    // Add event listener for opening and closing details
+    $('#beforeOut tbody').on('click', 'td.details-control', function () {
+        var tr = $(this).closest('tr');
+        var row = table.row( tr );
+
+        if ( row.child.isShown() ) {
+            // This row is already open - close it
+            row.child.hide();
+            tr.removeClass('shown');
+        }
+        else {
+            // Open this row
+            row.child( format(row.data()) ).show();
+            tr.addClass('shown');
+        }
+    } );
+} );
