e3afa013dd753172e9d014e88b247ba3d6682d0b
Carolina <carolina.fuentes@nottingham.ac.uk>
2017-12-20 13:16:41 +0000

    inbox datatable update

--- aiotbs3/public/javascripts/inbox.js
+++ aiotbs3/public/javascripts/inbox.js
@@ -2,6 +2,7 @@
 $(document).ready(function() {
     var getUserId = $("#HideUserId").val();
 
+
     var table = $('#beforeOut').DataTable( {
         "bFilter": false,
         "bInfo": false,
@@ -12,21 +13,31 @@ $(document).ready(function() {
             data: {userId: getUserId}
 
         },
+        select:"single",
         "columns": [
-
+            {
+                "className": 'details-control',
+                "orderable": false,
+                "data": null,
+                "defaultContent": '',
+                "render": function () {
+                    return '<i class="fa fa-plus-square" aria-hidden="true"></i>';
+                },
+                width:"15px"
+            },
             {data: "description"}
 
         ],
-
+        "order": [[1, 'asc']],
         "lengthChange": false,
         "length": 10,
         "paging": false,
-        "scrollY": '200px'
+        "scrollY": '300px'
     } );
 
 
 
-    var table = $('#afterOut').DataTable( {
+    var table2 = $('#afterOut').DataTable( {
         "bFilter": false,
         "bInfo": false,
         "ordering": false,
@@ -37,32 +48,112 @@ $(document).ready(function() {
 
         },
         "columns": [
-
+            {
+                "className": 'details-control',
+                "orderable": false,
+                "data": null,
+                "defaultContent": '',
+                "render": function () {
+                    return '<i class="fa fa-plus-square" aria-hidden="true"></i>';
+                },
+                width:"15px"
+            },
             {data: "description"}
 
         ],
-
+        "order": [[1, 'asc']],
         "lengthChange": false,
         "length": 10,
         "paging": false,
-        "scrollY": '200px'
+        "scrollY": '300px'
     } );
 
 
     // Add event listener for opening and closing details
     $('#beforeOut tbody').on('click', 'td.details-control', function () {
         var tr = $(this).closest('tr');
+        var tdi = tr.find("i.fa");
         var row = table.row( tr );
 
-        if ( row.child.isShown() ) {
+        if (row.child.isShown()) {
             // This row is already open - close it
             row.child.hide();
             tr.removeClass('shown');
+            tdi.first().removeClass('fa-minus-square');
+            tdi.first().addClass('fa-plus-square');
         }
         else {
             // Open this row
-            row.child( format(row.data()) ).show();
+            row.child(format(row.data())).show();
             tr.addClass('shown');
+            tdi.first().removeClass('fa-plus-square');
+            tdi.first().addClass('fa-minus-square');
         }
     } );
+
+    table.on("user-select", function (e, dt, type, cell, originalEvent) {
+        if ($(cell.node()).hasClass("details-control")) {
+            e.preventDefault();
+        }
+    });
+
+    // Add event listener for opening and closing details
+    $('#afterOut tbody').on('click', 'td.details-control', function () {
+        var tr = $(this).closest('tr');
+        var tdi = tr.find("i.fa");
+        var row = table.row( tr );
+
+        if (row.child.isShown()) {
+            // This row is already open - close it
+            row.child.hide();
+            tr.removeClass('shown');
+            tdi.first().removeClass('fa-minus-square');
+            tdi.first().addClass('fa-plus-square');
+        }
+        else {
+            // Open this row
+            row.child(formatAfter(row.data())).show();
+            tr.addClass('shown');
+            tdi.first().removeClass('fa-plus-square');
+            tdi.first().addClass('fa-minus-square');
+        }
+    } );
+
+    table2.on("user-select", function (e, dt, type, cell, originalEvent) {
+        if ($(cell.node()).hasClass("details-control")) {
+            e.preventDefault();
+        }
+    });
+
 } );
+
+
+function format(d){
+
+    // `d` is the original data object for the row
+    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
+        '<tr>' +
+        '</tr>' +
+        '<tr>' +
+        '<form>'+
+        '<input type="checkbox" name="value1" value="1"> Not Used'+
+        '<input type="checkbox" name="value2" value="1"> Forgot to scan out'+
+        '<input type="checkbox" name="value3" value="1"> Other '+
+        '</form>'
+        '</table>';
+}
+
+function formatAfter(d){
+
+    // `d` is the original data object for the row
+    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
+        '<tr>' +
+        '</tr>' +
+        '<tr>' +
+        '<form>'+
+        '<input type="checkbox" name="value4" value="1"> Not Used'+
+        '<input type="checkbox" name="value5" value="1"> Forgot to scan out'+
+        '<input type="checkbox" name="value6" value="1"> Other '+
+        '</form>'
+    '</table>';
+}
