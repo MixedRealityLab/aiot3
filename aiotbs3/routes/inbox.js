320de340e7deabaf0b2f1de782eca55b3853305f
Carolina <carolina.fuentes@nottingham.ac.uk>
2018-02-15 16:30:49 +0000

    work on grouping inbox view

--- aiotbs3/routes/inbox.js
+++ aiotbs3/routes/inbox.js
@@ -102,3 +102,62 @@ exports.getPredictionsFeedback =  function (userId,done) {
     });
 
 }
+
+
+//getting prediction to show as inbox notifications, adding grouped categories
+exports.getPredictionsFeedback2 =  function (userId,done) {
+    var dataBefore = [];
+    var dataAfter = [];
+
+    prediction.getPredictionsForUser(userId,function(err, data){
+        if(err){
+            console.log(err);
+            console.log("there was an error getting predictions from that specific user");
+            //res.send("there was an error see the console");
+        }
+        else {
+
+            //from data get inventory_id's
+            //identify if that inventory is grouped or not
+            //if isn't grouped
+                //go to complete data before/after array
+            //if is grouped
+                //get the category
+                //get all other inventory ids for that category
+                    //group by that category
+                    //identify if is data before or after
+
+
+
+            for (var i=0; i < data.length; i++) {
+                if (data[i].early_late ==0) {
+                    dataBefore.push({
+                        "product_id": data[i].product_id,
+                        "description": data[i].description,
+                        "prediction_id":data[i].id,
+                        "inventory_id":data[i].inventory_id
+                    });
+                } else {
+                    //scanned out after predicted date
+                    if (data[i].early_late ==1) {
+                        dataAfter.push({
+                            "product_id": data[i].product_id,
+                            "description": data[i].description,
+                            "prediction_id":data[i].id,
+                            "inventory_id":data[i].inventory_id
+
+                        });
+                    }
+                }
+
+            }
+
+            console.log(data);
+            //res.send(data);
+            var data= {"dataBefore":dataBefore,"dataAfter":dataAfter};
+            console.log(data);
+            return done(data);
+        }
+    });
+
+}
