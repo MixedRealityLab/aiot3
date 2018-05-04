de79c1a27b1a74a34c5ca975323e6efed21ba0ec
Carolina <carolina.fuentes@nottingham.ac.uk>
2018-05-04 17:42:16 +0100

    shopping list simplified

--- aiotbs3/public/javascripts/shoppingList.js
+++ aiotbs3/public/javascripts/shoppingList.js
@@ -36,6 +36,10 @@ $(document).ready(function() {
             {data: "description"},
             //{data: "stock_level"},//{data: "level"},
             {data: "stock"},
+            {
+                data: null,
+                defaultContent: "<button type='buttonEspecial' class='btn btn-light btn-sm'> <i class='glyphicon glyphicon-eye-close'></i> </button>"
+            }
 
 
         ],
@@ -52,104 +56,6 @@ $(document).ready(function() {
 
     //IN STOCK ESSENTIALS DATATABLE
 
-    $('#noAutonomous').on('shown.bs.modal', function () {
-
-        $.fn.dataTable.tables({visible: true, api: true}).columns.adjust();
-        var ListTableNoAutonomous = $('#shoppinglist_dataNA').DataTable({
-            "bFilter": false,
-            "bInfo": false,
-            "ordering": false,
-            "ajax": {
-                url: '/getInitialShoppingList',
-                type: 'POST',
-                data: {userId: getUserId}
-
-            },
-            "columns": [
-                {data: "description"},
-                //{data: "stock_level", className: 'editable'},//{data: "level"},
-                {data: "stock"},
-                //{
-                //    data: null,
-                //    defaultContent: "<button type='buttonEspecial' class='btn btn-success btn-sm'> <i class='glyphicon glyphicon-plus'></i> </button>"
-                //},
-                {
-                    data: null,
-                    defaultContent: "<button type='buttonEspecial' class='btn btn-danger btn-sm'> <i class='glyphicon glyphicon-trash'></i> </button>"
-                }
-
-            ],
-            //"order": [[1, 'asc']],
-            "lengthChange": false,
-            "length": 10,
-            "paging": false,
-            "destroy": true,
-            "scrollY": '300px',
-
-        });
-    });
-
-    $('#semiAutonomous').on('shown.bs.modal', function () {
-
-        $.fn.dataTable.tables({visible: true, api: true}).columns.adjust();
-        var ListTableSemiAutonomous = $('#shoppinglist_semiautonomous').DataTable({
-            "bFilter": false,
-            "bInfo": false,
-            "ordering": false,
-            "ajax": {
-                url: '/getInitialShoppingList',
-                type: 'POST',
-                data: {userId: getUserId}
-
-            },
-            "columns": [
-                {data: "description"},
-                //{data: "stock_level", className: 'editable'},//{data: "level"},
-                {data: "stock"},
-
-
-            ],
-            //"order": [[1, 'asc']],
-            "lengthChange": false,
-            "length": 10,
-            "paging": false,
-            "destroy": true,
-            "scrollY": '300px',
-
-        });
-    });
-
-
-
-    $('#Autonomous').on('shown.bs.modal', function () {
-
-        $.fn.dataTable.tables({visible: true, api: true}).columns.adjust();
-        var ListTableAutonomous = $('#shoppinglist_autonomous').DataTable({
-            "bFilter": false,
-            "bInfo": false,
-            "ordering": false,
-            "ajax": {
-                url: '/getInitialShoppingList',
-                type: 'POST',
-                data: {userId: getUserId}
-
-            },
-            "columns": [
-                {data: "description"},
-                //{data: "stock_level", className: 'editable'},//{data: "level"},
-                {data: "stock"},
-
-
-            ],
-            //"order": [[1, 'asc']],
-            "lengthChange": false,
-            "length": 10,
-            "paging": false,
-            "destroy": true,
-            "scrollY": '300px',
-
-        });
-    });
 
 
 });
